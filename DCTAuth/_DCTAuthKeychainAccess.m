c438b3c9161efd95210616cd0528178b6981779a
Daniel Tull <dt@danieltull.co.uk>
2013-06-17 15:49:59 -0700

    Use the query method to get the base query

--- DCTAuth/_DCTAuthKeychainAccess.m
+++ DCTAuth/_DCTAuthKeychainAccess.m
@@ -12,14 +12,11 @@
 
 + (NSArray *)accountDataForStoreName:(NSString *)storeName {
 	
-	NSDictionary *query = @{
-		(__bridge id)kSecClass       : (__bridge id)kSecClassGenericPassword,
-		(__bridge id)kSecAttrService : @"DCTAuth",
-		(__bridge id)kSecAttrLabel   : storeName,
-		(__bridge id)kSecAttrType    : @(_DCTAuthKeychainAccessTypeAccount),
-		(__bridge id)kSecReturnData  : (__bridge id)kCFBooleanTrue,
-		(__bridge id)kSecMatchLimit  : (__bridge id)kSecMatchLimitAll
-	};
+	NSMutableDictionary *query = [self queryForAccountIdentifier:nil storeName:storeName type:_DCTAuthKeychainAccessTypeAccount];
+	[query addEntriesFromDictionary:@{
+		(__bridge id)kSecReturnData : (__bridge id)kCFBooleanTrue,
+		(__bridge id)kSecMatchLimit : (__bridge id)kSecMatchLimitAll
+	}];
 
 	CFTypeRef result = NULL;
 	SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
@@ -31,7 +28,7 @@
 								  type:(_DCTAuthKeychainAccessType)type {
 
 	NSMutableDictionary *query = [self queryForAccountIdentifier:accountIdentifier storeName:storeName type:type];
-    SecItemDelete((__bridge CFDictionaryRef)query);
+	SecItemDelete((__bridge CFDictionaryRef)query);
 }
 
 + (void)addData:(NSData *)data
@@ -40,6 +37,7 @@ forAccountIdentifier:(NSString *)accountIdentifier
 		   type:(_DCTAuthKeychainAccessType)type {
 
 	NSMutableDictionary *query = [self queryForAccountIdentifier:accountIdentifier storeName:storeName type:type];
+	SecItemDelete((__bridge CFDictionaryRef)query);
 	query[(__bridge id)kSecValueData] = data;
 	SecItemAdd((__bridge CFDictionaryRef)query, NULL);
 }
@@ -64,16 +62,20 @@ forAccountIdentifier:(NSString *)accountIdentifier
 }
 
 + (NSMutableDictionary *)queryForAccountIdentifier:(NSString *)accountIdentifier storeName:(NSString *)storeName type:(_DCTAuthKeychainAccessType)type {
-	
+
+	NSAssert(storeName, @"storeName is required");
+
 	NSDictionary *query = @{
 		(__bridge id)kSecClass       : (__bridge id)kSecClassGenericPassword,
 		(__bridge id)kSecAttrService : @"DCTAuth",
 		(__bridge id)kSecAttrLabel   : storeName,
-		(__bridge id)kSecAttrAccount : accountIdentifier,
 		(__bridge id)kSecAttrType    : @(type)
 	};
 
-	return [query mutableCopy];
+	NSMutableDictionary *mQuery = [query mutableCopy];
+	if (accountIdentifier.length > 0) mQuery[(__bridge id)kSecAttrAccount] = accountIdentifier;
+
+	return mQuery;
 }
 
 @end
