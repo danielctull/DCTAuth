a7be0b95bb36e56c740bf89a673930490f9519ce
Daniel Tull <dt@danieltull.co.uk>
2013-06-18 11:57:47 +0100

    Add keychain tests

---
+++ DCTAuthTests/DCTAuthKeychainTests.m
@@ -0,0 +1,33 @@
+//
+//  DCTAuthKeychainTests.m
+//  DCTAuth
+//
+//  Created by Daniel Tull on 18/06/2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import "_DCTAuthKeychainAccess.h"
+#import <XCTest/XCTest.h>
+
+@interface DCTAuthKeychainTests : XCTestCase
+@end
+
+@implementation DCTAuthKeychainTests
+
+- (void)testDataStorage {
+
+	NSData *data = [@"Input string" dataUsingEncoding:NSUTF8StringEncoding];
+	NSString *account = @"Daniel";
+	NSString *storeName = @"Store";
+	_DCTAuthKeychainAccessType type = _DCTAuthKeychainAccessTypeAccount;
+
+	[_DCTAuthKeychainAccess addData:data forAccountIdentifier:account storeName:storeName type:type];
+
+	NSData *data2 = [_DCTAuthKeychainAccess dataForAccountIdentifier:account storeName:storeName type:type];
+	XCTAssertEqualObjects(data, data2, @"Data in is not the same as data out");
+
+	NSData *data3 = [_DCTAuthKeychainAccess dataForAccountIdentifier:account storeName:storeName type:_DCTAuthKeychainAccessTypeCredential];
+	XCTAssertFalse([data isEqualToData:data3], @"Credential data is same as account data");
+}
+
+@end
