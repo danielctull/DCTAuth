4598906d7ff33047daed343aae2b735d82e98789
Daniel Tull <dt@danieltull.co.uk>
2015-01-19 14:43:07 +0000

    Add DCTTestAccount

---
+++ DCTAuth Tests/DCTTestAccount.h
@@ -0,0 +1,13 @@
+//
+//  DCTTestAccount.h
+//  DCTAuth
+//
+//  Created by Daniel Tull on 19.01.2015.
+//  Copyright (c) 2015 Daniel Tull. All rights reserved.
+//
+
+@import DCTAuth;
+
+@interface DCTTestAccount : DCTAuthAccount
+
+@end
---
+++ DCTAuth Tests/DCTTestAccount.m
@@ -0,0 +1,23 @@
+//
+//  DCTTestAccount.m
+//  DCTAuth
+//
+//  Created by Daniel Tull on 19.01.2015.
+//  Copyright (c) 2015 Daniel Tull. All rights reserved.
+//
+
+#import "DCTTestAccount.h"
+
+@implementation DCTTestAccount
+
+#pragma mark - DCTAuthAccountSubclass
+
+- (void)signURLRequest:(NSMutableURLRequest *)request {}
+
+- (void)authenticateWithHandler:(void(^)(NSArray *responses, NSError *error))handler {}
+
+- (void)reauthenticateWithHandler:(void(^)(DCTAuthResponse *response, NSError *error))handler {}
+
+- (void)cancelAuthentication {}
+
+@end
