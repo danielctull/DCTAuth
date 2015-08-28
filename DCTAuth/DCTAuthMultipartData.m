84e15c943d7fc7fffd398b6f12ef61c27afcdd0c
Daniel Tull <dt@danieltull.co.uk>
2015-08-28 08:34:12 +0100

    Resolve nullability warnings

--- DCTAuth/DCTAuthMultipartData.m
+++ DCTAuth/DCTAuthMultipartData.m
@@ -8,17 +8,29 @@
 
 #import "DCTAuthMultipartData.h"
 
+@interface NSMutableData (DCTAuth)
+- (void)dctauth_appendData:(NSData *)data;
+@end
+
 @implementation DCTAuthMultipartData
 
 - (NSData *)dataWithBoundary:(NSString *)boundary {
 	NSMutableData *body = [NSMutableData new];
-	[body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-	[body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n", self.name] dataUsingEncoding:NSUTF8StringEncoding]];
-	[body appendData:[[NSString stringWithFormat:@"Content-Type: %@\r\n", self.type] dataUsingEncoding:NSUTF8StringEncoding]];
-	[body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-	[body appendData:self.data];
-	[body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+	[body dctauth_appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+	[body dctauth_appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n", self.name] dataUsingEncoding:NSUTF8StringEncoding]];
+	[body dctauth_appendData:[[NSString stringWithFormat:@"Content-Type: %@\r\n", self.type] dataUsingEncoding:NSUTF8StringEncoding]];
+	[body dctauth_appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+	[body dctauth_appendData:self.data];
+	[body dctauth_appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
 	return [body copy];
 }
 
+@end
+
+@implementation NSMutableData (DCTAuth)
+- (void)dctauth_appendData:(NSData *)data {
+	if (data) {
+		[self appendData:data];
+	}
+}
 @end
